61a082a769b6a4f8547b875f8b2326315a33213b
Marius Appel <marius.appel@uni-muenster.de>
2019-01-04 17:22:43 +0100

    added build_info.h for non cmake builds

---
+++ src/build_info.h
@@ -0,0 +1,15 @@
+// build_info.h.in will be used by CMake to create build_info.h with correction project version constant definitions
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define GDALCUBES_VERSION_MAJOR 0
+#define GDALCUBES_VERSION_MINOR 0
+#define GDALCUBES_VERSION_PATCH 1
+#define GDALCUBES_GIT_DESC "bb52c6e2"
+#define GDALCUBES_GIT_COMMIT "bb52c6e2d122b3843ef1b0e4fd5754d856ba80c0"
+
+#define COLLECTION_FORMAT_VERSION_MAJOR 
+#define COLLECTION_FORMAT_VERSION_MINOR 
+#define COLLECTION_FORMAT_VERSION_PATCH 
+
+#endif // BUILD_INFO_H

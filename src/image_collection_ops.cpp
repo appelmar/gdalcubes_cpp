305ef298c5066a8e468017665d5d1051ce4f23bb
Marius Appel <marius.appel@uni-muenster.de>
2020-08-03 10:35:18 +0200

    add translate_COG batch operation

--- src/image_collection_ops.cpp
+++ src/image_collection_ops.cpp
@@ -24,7 +24,7 @@
 
 namespace gdalcubes {
 
-void image_collection_ops::translate_gtiff(std::shared_ptr<gdalcubes::image_collection> in, std::string out_dir, uint16_t nthreads, bool force, std::vector<std::string> creation_options) {
+void image_collection_ops::translate_gtiff(std::shared_ptr<gdalcubes::image_collection> in, std::string out_dir, uint16_t nthreads, bool overwrite, std::vector<std::string> creation_options) {
     if (!filesystem::exists(out_dir)) {
         filesystem::mkdir_recursive(out_dir);
     }
@@ -43,51 +43,24 @@ void image_collection_ops::translate_gtiff(std::shared_ptr<gdalcubes::image_coll
     std::mutex mutex;
     std::vector<image_collection::gdalrefs_row> gdalrefs = in->get_gdalrefs();
 
-    bool has_COG = GetGDALDriverManager()->GetDriverByName("COG") != NULL;
-
 
     for (uint16_t it = 0; it < nthreads; ++it) {
-        thrds.push_back(std::thread([it, nthreads, &out_dir, &gdalrefs, &prg, in, &mutex, force, has_COG, &creation_options]() {
+        thrds.push_back(std::thread([it, nthreads, &out_dir, &gdalrefs, &prg, in, &mutex, overwrite, &creation_options]() {
             for (uint32_t i = it; i < gdalrefs.size(); i += nthreads) {
                 prg->increment((double)1 / (double)gdalrefs.size());
                 std::string descr = gdalrefs[i].descriptor;
 
                 CPLStringList translate_args;
 
-                if (has_COG) {
-                    translate_args.AddString("-of");
-                    translate_args.AddString("COG");
-                }
-                else {
-                    translate_args.AddString("-of");
-                    translate_args.AddString("GTiff");
-
-                    translate_args.AddString("-co");
-                    translate_args.AddString("TILED=YES");
-
-                    translate_args.AddString("-co");
-                    translate_args.AddString("COPY_SRC_OVERVIEWS=YES");
-
-                    translate_args.AddString("-co");
-                    translate_args.AddString("COMPRESS=DEFLATE");
-
-                    translate_args.AddString("-co");
-                    translate_args.AddString("ZLEVEL=2");
-                }
-
-
+                translate_args.AddString("-of");
+                translate_args.AddString("GTiff");
                 for (auto it = creation_options.begin(); it != creation_options.end(); ++it) {
                     translate_args.AddString("-co");
                     translate_args.AddString(it->c_str());
                 }
 
-
                 translate_args.AddString("-b");
                 translate_args.AddString(std::to_string(gdalrefs[i].band_num).c_str());  // band_num is 1 based
-
-                //                for (uint16_t i = 0; i < gdal_translate_args.size(); ++i) {
-                //                    translate_args.AddString(gdal_translate_args[i].c_str());
-                //                }
 
                 GDALTranslateOptions* trans_options = GDALTranslateOptionsNew(translate_args.List(), NULL);
                 if (trans_options == NULL) {
@@ -105,8 +78,8 @@ void image_collection_ops::translate_gtiff(std::shared_ptr<gdalcubes::image_coll
                     filesystem::mkdir(outimgdir);
                 }
                 std::string outfile = filesystem::join(outimgdir, std::to_string(gdalrefs[i].band_id) + ".tif");
-                if (filesystem::exists(outfile) && !force) {
-                    GCBS_DEBUG(outfile + " already exists; set force=true to force recreation of existing files.");
+                if (filesystem::exists(outfile) && !overwrite) {
+                    GCBS_DEBUG(outfile + " already exists; set overwrite=true to force recreation of existing files.");
                 } else {
                     GDALDatasetH out = GDALTranslate(outfile.c_str(), (GDALDatasetH)dataset, trans_options, NULL);
                     if (!out) {
@@ -135,6 +108,109 @@ void image_collection_ops::translate_gtiff(std::shared_ptr<gdalcubes::image_coll
     }
     prg->finalize();
 }
+
+
+
+
+
+void image_collection_ops::translate_cog(std::shared_ptr<gdalcubes::image_collection> in, std::string out_dir, uint16_t nthreads, bool overwrite, std::vector<std::string> creation_options) {
+    if (!filesystem::exists(out_dir)) {
+        filesystem::mkdir_recursive(out_dir);
+    }
+
+    if (!filesystem::is_directory(out_dir)) {
+        throw std::string("ERROR in image_collection_ops::translate_cog(): output is not a directory.");
+    }
+
+    std::vector<std::thread> thrds;
+
+    std::shared_ptr<progress> prg = config::instance()->get_default_progress_bar()->get();
+    prg->set(0);  // explicitly set to zero to show progress bar immediately
+
+    in->write(filesystem::join(out_dir, filesystem::filename(in->get_filename())));
+
+    std::mutex mutex;
+    std::vector<image_collection::gdalrefs_row> gdalrefs = in->get_gdalrefs();
+
+    bool has_COG = GetGDALDriverManager()->GetDriverByName("COG") != NULL;
+    if (!has_COG) {
+        throw std::string("Direct translation to COG requires GDAL >= 3.1, please combine translate_gtiff and create_overviews instead");
+    }
+
+    for (uint16_t it = 0; it < nthreads; ++it) {
+        thrds.push_back(std::thread([it, nthreads, &out_dir, &gdalrefs, &prg, in, &mutex, overwrite, &creation_options]() {
+          for (uint32_t i = it; i < gdalrefs.size(); i += nthreads) {
+              prg->increment((double)1 / (double)gdalrefs.size());
+              std::string descr = gdalrefs[i].descriptor;
+
+              CPLStringList translate_args;
+              translate_args.AddString("-of");
+              translate_args.AddString("COG");
+
+              for (auto it = creation_options.begin(); it != creation_options.end(); ++it) {
+                  translate_args.AddString("-co");
+                  translate_args.AddString(it->c_str());
+              }
+
+              translate_args.AddString("-b");
+              translate_args.AddString(std::to_string(gdalrefs[i].band_num).c_str());  // band_num is 1 based
+
+
+              GDALTranslateOptions* trans_options = GDALTranslateOptionsNew(translate_args.List(), NULL);
+              if (trans_options == NULL) {
+                  GCBS_WARN("Cannot create gdal_translate options.");
+                  continue;
+              }
+              GDALDataset* dataset = (GDALDataset*)GDALOpen(descr.c_str(), GA_ReadOnly);
+              if (!dataset) {
+                  GCBS_WARN("Cannot open GDAL dataset '" + descr + "'.");
+                  GDALTranslateOptionsFree(trans_options);
+                  continue;
+              }
+              std::string outimgdir = filesystem::join(out_dir, std::to_string(gdalrefs[i].image_id));
+              if (!filesystem::exists(outimgdir)) {
+                  filesystem::mkdir(outimgdir);
+              }
+
+              std::string outfile = filesystem::join(outimgdir, std::to_string(gdalrefs[i].band_id) + ".tif");
+              if (filesystem::exists(outfile) && !overwrite) {
+                  GCBS_DEBUG(outfile + " already exists; set overwrite=true to force recreation of existing files.");
+              } else {
+                  GDALDatasetH out = GDALTranslate(outfile.c_str(), (GDALDatasetH)dataset, trans_options, NULL);
+                  if (!out) {
+                      GCBS_WARN("Cannot translate GDAL dataset '" + descr + "'.");
+                      GDALClose((GDALDatasetH)dataset);
+                      GDALTranslateOptionsFree(trans_options);
+                  }
+                  GDALClose(out);
+                  GDALTranslateOptionsFree(trans_options);
+              }
+              GDALClose((GDALDatasetH)dataset);
+
+              // Run SQL update anyway to fix broken links etc. if needed
+              std::string sql = "UPDATE gdalrefs SET descriptor='" + outfile + "', band_num=1 " + "WHERE image_id=" + std::to_string(gdalrefs[i].image_id) + " AND band_id=" + std::to_string(gdalrefs[i].band_id) + ";";
+
+              mutex.lock();
+              if (sqlite3_exec(in->get_db_handle(), sql.c_str(), NULL, NULL, NULL) != SQLITE_OK) {
+                  GCBS_WARN("Skipping image " + std::to_string(gdalrefs[i].image_id) + " due to failed band table update");
+              }
+              mutex.unlock();
+          }
+        }));
+    }
+    for (uint16_t it = 0; it < nthreads; ++it) {
+        thrds[it].join();
+    }
+    prg->finalize();
+}
+
+
+
+
+
+
+
+
 
 void image_collection_ops::create_overviews(std::shared_ptr<image_collection> in, std::vector<int> levels, std::string resampling, uint16_t nthreads) {
     std::vector<image_collection::gdalrefs_row> gdalrefs = in->get_gdalrefs();

23f42d551cab292e83241850896b5af28d1fd33e
Marius Appel <marius.appel@uni-muenster.de>
2021-02-15 12:52:14 +0100

    fix GH actions ubuntu-16.04 build

--- .github/workflows/ubuntu-16.04/install_dependencies.sh
+++ .github/workflows/ubuntu-16.04/install_dependencies.sh
@@ -2,6 +2,7 @@
 sudo apt-get update
 sudo apt-get install -y libboost-all-dev cmake g++ libsqlite3-dev software-properties-common
 sudo apt-get install -y libnetcdf-dev libcurl4-openssl-dev libcpprest-dev doxygen graphviz
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160
 sudo add-apt-repository ppa:ubuntugis/ppa
 sudo apt-get update
 sudo apt-get install -y libxml2-dev libopenjp2-7-dev libhdf4-alt-dev libgdal-dev gdal-bin libproj-dev

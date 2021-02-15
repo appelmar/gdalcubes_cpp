8c1052a4535f26ed559ffe28679f772264f74d76
Marius Appel <marius.appel@uni-muenster.de>
2021-02-15 11:57:15 +0100

    Create install_dependencies.sh

---
+++ .github/workflows/ubuntu-16.04/install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get update
+apt-get install -y libboost-all-dev cmake g++ libsqlite3-dev 
+apt-get install -y libnetcdf-dev libcurl4-openssl-dev libcpprest-dev doxygen graphviz
+apt-get install -y libxml2-dev libopenjp2-7-dev libhdf4-alt-dev libgdal-dev gdal-bin libproj-dev
